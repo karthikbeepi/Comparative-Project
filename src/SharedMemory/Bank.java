e08fac306494f221976ad5b7d56adaf944efa5f4
BeePi <karthikbeepi@gmail.com>
2019-06-20 13:29:05 -0400

    Simply goofing around

---
+++ src/SharedMemory/Bank.java
@@ -0,0 +1,120 @@
+package SharedMemory;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Bank implements Runnable {
+
+	String bankName;
+	Integer balance;
+	LinkedBlockingQueue<String> q;
+	ArrayList<String> customersDone = new ArrayList<>();
+	ArrayList<String> customers = new ArrayList<>();
+	ArrayList<String> bankMailBox;
+	public Bank(String s, Integer integer, Set<String> set, ArrayList<String> bankMailBox) {
+		bankName = s;
+		balance = integer;
+		customers = new ArrayList<>();
+		for(String temp : set)
+			customers.add(temp);
+		this.bankMailBox = bankMailBox;
+	}
+
+	@Override
+	public void run() {
+		
+		startUDPConnectionDual();
+		try {
+			//wait(new Random().nextInt(100));
+			Thread.sleep(new Random().nextInt(100)+10);
+			} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		synchronized (System.out) {
+			System.out.println("\n"+bankName+" has "+balance+" dollar(s) remaining !");
+		}
+		
+	}
+
+	void startUDPConnectionDual() {
+//		DatagramSocket aSocket = null;
+//		try {
+//			aSocket = new DatagramSocket(udpPort);
+//			byte[] buffer = new byte[1000];		
+//			while (customersDone.size()!=customers.size()) {
+//				
+//				buffer = new byte[1000];
+//				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+//				
+//				aSocket.receive(request);
+//				
+//				String req = new String(request.getData());
+//				
+//				String replyStr = replyGenerator(req.trim());
+//				
+//				byte[] rep = new byte[1000];
+//				rep = replyStr.getBytes();
+//				DatagramPacket reply = new DatagramPacket(rep, replyStr.length(), request.getAddress(),
+//						request.getPort());
+//				
+//				aSocket.send(reply);
+//				
+//			}
+//		} catch (SocketException e) {
+//			System.out.println("Socket: " + e.getMessage());
+//		} catch (IOException e) {
+//			System.out.println("IO: " + e.getMessage());
+//		} finally {
+//			if (aSocket != null)
+//				aSocket.close();
+//		}
+		
+		while (customersDone.size()!=customers.size()) {
+			synchronized (bankMailBox) {
+				while(bankMailBox.size()>0)
+				{
+					String req = bankMailBox.get(0);
+					String rep = replyGenerator(req);
+					
+				}
+			}
+		}
+		
+	}
+
+	private String replyGenerator(String s) {
+		
+		String sp[]= s.split(" ");
+		
+		if(sp[0].compareToIgnoreCase("DONE")==0)
+		{
+			customersDone.add(sp[1]);
+			return "Updated customers done "+bankName;
+		}
+		
+		else if(sp[0].compareToIgnoreCase("request")==0)
+		{
+			int reqAmt = Integer.parseInt(sp[2]);
+			if(reqAmt<=balance)
+			{
+				balance-=reqAmt;
+				System.out.println(bankName+" approves the loan of "+reqAmt+" from "+sp[1]);
+				return "YES";
+			}
+			else
+			{
+				System.out.println(bankName+" denies the loan of "+reqAmt+" from "+sp[1]);
+				return "NO";
+			}
+				
+		}
+		
+		return null;
+	}
+}
---
+++ src/SharedMemory/Customer.java
@@ -0,0 +1,124 @@
+package SharedMemory;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Set;
+
+public class Customer implements Runnable {
+
+	String customerName;
+	Integer intialLoanAmount;
+	Integer loanAmt;
+	ArrayList<String> bankNames;
+	ArrayList<String> banksDelisted = new ArrayList<>();
+	ArrayList<String> customersDone;
+	
+	public Customer(String s, Integer integer,  Set<String> set) {
+		customerName = s;
+		loanAmt = integer;
+		intialLoanAmount = integer;
+		bankNames = new ArrayList<>();
+		for(String temp : set)
+			bankNames.add(temp);
+	}
+
+	@Override
+	public void run() {
+		
+		while(loanAmt>0&&banksDelisted.size()<bankNames.size())
+		{
+			Random rand = new Random();
+			int reqAmt = rand.nextInt(50)+1;
+//			reqAmt = 50;
+			if(reqAmt>loanAmt)
+				reqAmt = loanAmt;
+			int bankNo = rand.nextInt(bankNames.size());
+			String bankSelected = bankNames.get(bankNo);
+			while(banksDelisted.contains(bankSelected))
+			{
+				bankNo = rand.nextInt(bankNames.size());
+				bankSelected = bankNames.get(bankNo);
+			}
+			System.out.println(customerName+" requests a loan of "+reqAmt+" dollar(s) from "+bankSelected);
+			
+			String response = requestBanks("request "+customerName+" "+reqAmt, 7570+bankNo);
+			try {
+//				wait(new Random().nextInt(100));
+				Thread.sleep(new Random().nextInt(100)+10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			if(response.trim().compareToIgnoreCase("NO")==0)
+			{
+				banksDelisted.add(bankSelected);
+				continue;
+			}
+			else
+				loanAmt-=reqAmt;
+			try {
+//				wait(new Random().nextInt(100));
+				Thread.sleep(new Random().nextInt(100)+10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		try {
+//			wait(new Random().nextInt(100));
+			Thread.sleep(new Random().nextInt(100));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		synchronized (System.out) {
+			if(loanAmt==0)
+				System.out.println("\n"+customerName+" has reached the objective of "+intialLoanAmount+" Woo Hoo!");
+			else
+				System.out.println("\n"+customerName+" was only able to borrow "+(intialLoanAmount-loanAmt)+" Boo Hoo!!! ");
+		}
+		for(int i=7570; i<7570+bankNames.size(); i++)
+		{
+			requestBanks("DONE "+customerName, i);
+		}
+	}
+	
+	public String requestBanks(String str, int port) {
+		DatagramSocket aSocket = null; 	
+		String s= "";
+		try{
+			aSocket = new DatagramSocket(); 
+			byte [] message = str.getBytes(); 
+			InetAddress aHost = InetAddress.getByName("localhost"); 
+			int serverPort = port;
+			DatagramPacket request = new DatagramPacket(message, str.length(), aHost, serverPort);//request packet read
+			
+			aSocket.send(request);
+			
+			byte [] buffer = new byte[1000];
+			DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+			aSocket.receive(reply);
+			String repMSG = new String(reply.getData());
+
+			if(repMSG.charAt(0)!='b')
+				s = s.concat(repMSG);
+		}
+		catch(SocketException e){
+			System.out.println("Socket: "+e.getMessage());
+		}
+		catch(IOException e){
+			e.printStackTrace();
+			System.out.println("IO: "+e.getMessage());
+		}
+		finally{
+			if(aSocket != null) aSocket.close();
+		}
+		return s.toString();
+	}
+
+
+}
